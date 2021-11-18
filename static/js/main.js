6fe13ea305545d413a8eb0749d43b1e116c8e798
Will Holmes <willholmes95@gmail.com>
2021-11-18 18:38:24 -0500

    Add root and navbar styles, update list and section templates

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,11 @@
 // Test JavaScript
 console.log('JavaScript is loaded!')
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+let navBar = document.getElementById('js-navbar');
+
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active-nav');
+    navBar.classList.toggle('active-nav');
+});
