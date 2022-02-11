12c5ac4d72874c3a75853190b860d17777cbd46f
Will Holmes <willholmes95@gmail.com>
2022-02-10 21:47:37 -0500

    Click to toggle submenu on mobile instead of hover

--- static/js/main.js
+++ static/js/main.js
@@ -4,7 +4,18 @@ let navBar = document.getElementById('js-navbar');
 let mainMenu = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar__toggle');
 
+let menuItems = document.querySelectorAll('.menu__item');
+
+// Show or hide nav on click of menu burger
 navBarToggle.addEventListener('click', function () {
     navBar.classList.toggle('main-nav--active');
     mainMenu.classList.toggle('main-nav--active');    
+});
+
+// Show or hide menu items on mobile
+menuItems.forEach(function(item) {
+    item.addEventListener('click', function () {
+        let subMenu = item.querySelector('.sub-menu');
+        subMenu.classList.toggle('sub-menu--active');
+    });
 });
